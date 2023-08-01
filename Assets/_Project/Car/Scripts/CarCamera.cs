96668abbd7ce9b59fa49796ea99df23c90a0d978
Alex Bilesck <alex.pontes@facens.br>
2023-08-01 19:52:58 -0300

    updated camera controller

--- Assets/_Project/Car/Scripts/CarCamera.cs
+++ Assets/_Project/Car/Scripts/CarCamera.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 
 public class CarCamera : MonoBehaviour
 {
-    [Header("Effects")]
-    [SerializeField] private float minFov = 40;
-    [SerializeField] private float maxFov = 100;
-    [Header("Position")]
-    [SerializeField] private float moveSmoothness;
-    [SerializeField] private Vector3 startMoveOffset;
-    [SerializeField] private Vector3 endMoveOffset;
-    [Header("Rotation")]
-    [SerializeField] private float rotationSmoothness;
-    [SerializeField] private Vector3 startRotationOffset;
-    [SerializeField] private Vector3 endRotationOffset;
+    //[Header("Effects")]
+    //[SerializeField] private float minFov = 40;
+    //[SerializeField] private float maxFov = 100;
+    //[Header("Position")]
+    //[SerializeField] private float moveSmoothness;
+    //[SerializeField] private Vector3 endMoveOffset;
+    //[Header("Rotation")]
+    //[SerializeField] private float rotationSmoothness;
+    //[SerializeField] private Vector3 startRotationOffset;
+    //[SerializeField] private Vector3 endRotationOffset;
 
-    [SerializeField] private Transform car;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody car;
+    [SerializeField] private Transform targetLook;
 
     private Camera cam;
     private Transform target;
     private Transform myTransform;
-    private Vector3 moveOffset;
-    private Vector3 rotationOffset;
+    //private Vector3 moveOffset;
+    //private Vector3 rotationOffset;
 
     private void Awake()
     {
@@ -32,37 +34,43 @@ public class CarCamera : MonoBehaviour
         target = car.transform;
     }
 
+    //private void FixedUpdate()
+    //{
+    //    handleMovement();
+    //    HandleRotation();
+    //}
+
     private void FixedUpdate()
     {
-        handleMovement();
-        HandleRotation();
+        //HandleSpeed();
+        Vector3 playerForward = (car.velocity + target.forward).normalized;
+        myTransform.position = Vector3.Lerp(myTransform.position,
+            target.position + target.TransformVector(offset)
+            + playerForward * (-5f),
+            speed * Time.deltaTime);
+        myTransform.LookAt(targetLook);
     }
 
-    private void LateUpdate()
-    {
-        HandleSpeed();
-    }
+    //private void HandleSpeed()
+    //{
+    //    float lerpSpeed = 0; // car.GetNormalizedSpeed();
+    //    cam.fieldOfView = Mathf.Lerp(minFov, maxFov, lerpSpeed);
+    //    moveOffset = Vector3.Lerp(startMoveOffset, endMoveOffset, lerpSpeed);
+    //    rotationOffset = Vector3.Lerp(startRotationOffset, endRotationOffset, lerpSpeed);
+    //}
 
-    private void HandleSpeed()
-    {
-        float lerpSpeed = 0; // car.GetNormalizedSpeed();
-        cam.fieldOfView = Mathf.Lerp(minFov, maxFov, lerpSpeed);
-        moveOffset = Vector3.Lerp(startMoveOffset, endMoveOffset, lerpSpeed);
-        rotationOffset = Vector3.Lerp(startRotationOffset, endRotationOffset, lerpSpeed);
-    }
+    //private void handleMovement()
+    //{
+    //    Vector3 targetPos = target.TransformPoint(moveOffset);
 
-    private void handleMovement()
-    {
-        Vector3 targetPos = target.TransformPoint(moveOffset);
+    //    myTransform.position = Vector3.Lerp(myTransform.position, targetPos, moveSmoothness);
+    //}
 
-        myTransform.position = Vector3.Lerp(myTransform.position, targetPos, moveSmoothness);
-    }
+    //private void HandleRotation()
+    //{
+    //    var direction = target.position - myTransform.position;
+    //    Quaternion rot = Quaternion.LookRotation(direction + rotationOffset, Vector3.up);
 
-    private void HandleRotation()
-    {
-        var direction = target.position - myTransform.position;
-        Quaternion rot = Quaternion.LookRotation(direction + rotationOffset, Vector3.up);
-
-        myTransform.rotation = Quaternion.Lerp(myTransform.rotation, rot, rotationSmoothness);
-    }
+    //    myTransform.rotation = Quaternion.Lerp(myTransform.rotation, rot, rotationSmoothness);
+    //}
 }
